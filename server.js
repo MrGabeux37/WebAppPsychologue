c0149677d6578ab0bac7f5e92a837e7d59d3ae4c
Gabriel Lacroix <glacroix0@live.ca>
2019-09-16 17:16:06 -0400

    Installation Dependencies et Setup serveur sur HTML

---
+++ server.js
@@ -0,0 +1,26 @@
+const Hapi = require('hapi');
+
+const server = new Hapi.Server({
+  host: 'localhost',
+  port: 3000
+});
+
+async function start (){
+  await server.register({
+    plugin: require('inert')
+  });
+  await server.start()
+  console.log('Server running at: '+server.info.uri);
+};
+
+server.route([{
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => {
+    return h.file('D:/Git/WebAppPsychologue/view/home.html');
+    }
+  }
+]);
+
+
+start();
