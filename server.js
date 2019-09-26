0aaf1a71d4b2e6b1a5fbc8aae3a5deec5f878f4b
Gabriel Lacroix <glacroix0@live.ca>
2019-09-26 19:53:46 -0400

    Connection DB
    Premiere tentative de connection de la Base de Donnees MySQL.

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
+'use strict'
+
 const Hapi = require('hapi');
+const Mysql = require('mysql');
+
+
+var connection = Mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'password',
+  database:'hapijslogin'
+});
+
+
+
+connection.connect(function(err){
+  if(err){
+    console.error('error connecting: ' + err.stack);
+    return
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
 
 const server = new Hapi.Server({
   host: 'localhost',
@@ -18,7 +40,23 @@ async function start (){
     handler: (request, h) => {
       return h.view('index');
       }
-  }]);
+  },
+  {
+    method: 'POST',
+    path: '/register',
+    handler: (request, h) =>{
+      const payload = request.query;
+      console.log(payload.first);
+
+      connection.query('INSERT INTO users (first_name,last_name,email,password) VALUES ("' + payload.first + '","' + payload.last_name + '","' + payload.email + '","' + payload.password + '")', function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+            return results;
+      })
+      return payload
+    }
+  }
+  ]);
 
   server.views({
     engines:{
