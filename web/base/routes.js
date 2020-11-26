859f8aff57bb87f32f8e555ac5bc7bdc99c81acc
Gabriel Lacroix <glacroix0@live.ca>
2020-11-25 20:02:59 -0500

    Clean up fichier Routes.js
    Enlever les declarations inutiles.

--- web/base/routes.js
+++ web/base/routes.js
@@ -1,16 +1,5 @@
 'use strict'
 
-const Handlers = require('./handler');
-const Mysql = require('mysql');
-const Boom = require('boom');
-const Jsdom = require('jsdom');
-const md5 = require('md5');
-const Sequelize =require('sequelize');
-const Op = Sequelize.Op;
-const Client = require('./models/client.js');
-const Psychologue = require('./models/psychologue.js');
-const PlageHoraire = require('./models/plagehoraire.js');
-const RendezVous = require('./models/rendezvous.js');
 var routeBase = require('./routing/base.js');
 var routePsycho = require('./routing/routePsycho.js');
 var routeClient = require('./routing/routeClient.js');
