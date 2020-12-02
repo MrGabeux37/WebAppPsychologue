b13d339fa30ea8ee0263aad4e383ba5249bf415d
Gabriel Lacroix <glacroix0@live.ca>
2020-12-01 20:01:24 -0500

    Squelette du Calendrier
    implémentation du squelette du calendrier en CSS, JS et html dans la section psychologue.

--- public/script/calendrierPsycho.js
+++ public/script/calendrierPsycho.js
@@ -5,13 +5,38 @@
 window.onload = init;
 
   function init(){
+
     const cal = document.getElementById("calendrierPsycho");
     console.log(cal);
-    for(let day = 1 ; day<=31 ; day++){
-      cal.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`);
-      console.log(day);
+
+    const isWeekend = day => {
+      return day%7===0 || day%7===6;
+    }
+
+    for(let day = 1 ; day<=35 ; day++){
+
+      const date = new Date(Date.UTC(2018,0,day));
+      const options = {weekday : "short"}
+      var dayname;
+
+      if(day<=7){
+        dayName = new Intl.DateTimeFormat("en-US",options).format(date);
+      }
+      else dayName="";
+      const weekend = isWeekend(day);
+      console
+
+      cal.insertAdjacentHTML("beforeend",`<div class="day ${weekend ? "weekend" : ""}"><div class="name">${dayName}</div>${day}</div>`);
 
     }
+
+    document.querySelectorAll("#calendrierPsycho .day").forEach(day => {
+        day.addEventListener("click", event => {
+          event.currentTarget.classList.toggle("selected");
+        });
+    });
+
+
   }
 
 })(window, document, undefined);
