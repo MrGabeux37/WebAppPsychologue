cfd26125ada2f1c8381a01f0b0ca39134d3f0f30
Gabriel Lacroix <glacroix0@live.ca>
2020-12-06 16:04:30 -0500

    séparation des nom de jour du calendrier.

--- public/script/calendrierPsycho.js
+++ public/script/calendrierPsycho.js
@@ -13,20 +13,20 @@ window.onload = init;
       return day%7===0 || day%7===6;
     }
 
+    for(let week=1;week<=7;week++){
+
+      const date = new Date(Date.UTC(2018,0,week));
+      const options = {weekday : "short"}
+      var dayname = new Intl.DateTimeFormat("en-US",options).format(date);
+
+      cal.insertAdjacentHTML("beforeend",`<div class="name">${dayname}</div>`);
+    }
+
     for(let day = 1 ; day<=35 ; day++){
 
-      const date = new Date(Date.UTC(2018,0,day));
-      const options = {weekday : "short"}
-      var dayname;
-
-      if(day<=7){
-        dayName = new Intl.DateTimeFormat("en-US",options).format(date);
-      }
-      else dayName="";
       const weekend = isWeekend(day);
-      console
 
-      cal.insertAdjacentHTML("beforeend",`<div class="day ${weekend ? "weekend" : ""}"><div class="name">${dayName}</div>${day}</div>`);
+      cal.insertAdjacentHTML("beforeend",`<div class="day ${weekend ? "weekend" : ""}">${day}</div>`);
 
     }
 
